842a398c169bc6c01435c69482ba2b2608665475
HudeyfaParriera98 <103253765+HudeyfaParriera98@users.noreply.github.com>
2023-11-21 07:39:05 +0100

    Update DecLottery.sol

--- DecLottery.sol
+++ DecLottery.sol
@@ -1,1 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract DecentralizedLottery {
+    address public admin;
+    uint256 public ticketPrice;
+    uint256 public lotteryEndTime;
+    bool public lotteryClosed;
+    address[] public participants;
+    mapping(address => bool) public hasParticipated;
+
+    event LotteryWinner(address winner, uint256 prize);
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can call this function");
+        _;
+    }
+
+    modifier onlyDuringLottery() {
+        require(block.timestamp < lotteryEndTime, "Lottery has ended");
+        _;
+    }
+
+    modifier onlyAfterLottery() {
+        require(block.timestamp >= lotteryEndTime, "Lottery has not ended");
+        _;
+    }
+
+    constructor(uint256 _ticketPrice, uint256 _lotteryDuration) {
+        admin = msg.sender;
+        ticketPrice = _ticketPrice;
+        lotteryEndTime = block.timestamp + _lotteryDuration;
+    }
+
+    function participate() external payable onlyDuringLottery {
+        require(msg.value == ticketPrice, "Incorrect ticket price");
+        require(!hasParticipated[msg.sender], "Already participated");
+
+        participants.push(msg.sender);
+        hasParticipated[msg.sender] = true;
+    }
+
+    function closeLottery() external onlyAdmin onlyDuringLottery {
+        lotteryClosed = true;
+    }
+
+    function conductLottery() external onlyAdmin onlyAfterLottery {
+        require(lotteryClosed, "Lottery is still open");
+
+        uint256 participantCount = participants.length;
+
+        // Ensure there are participants
+        require(participantCount > 0, "No participants");
+
+        // Select a random winner
+        uint256 randomIndex = generateRandomNumber(participantCount);
+        address winner = participants[randomIndex];
+
+        // Calculate prize (assuming all funds are distributed)
+        uint256 prize = address(this).balance;
+
+        // Transfer prize to the winner
+        payable(winner).transfer(prize);
+
+        // Emit winner event
+        emit LotteryWinner(winner, prize);
+    }
+
+    
+}
